77f30a629724211b2bdb0c68ba4d28d95cb362d4
Nyx Olcott <therivernyx@arcaneasylumstudios.com>
2024-07-18 04:46:07 -0600

    cleared repo

--- Assets/Scripts/Scriptable Objects/Item.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
-public class Item : ScriptableObject
-{
-    public string itemName = "New Item";    // Name of the item
-    public Sprite icon = null;              // Icon of the item
-    //Other item values here!
-}
