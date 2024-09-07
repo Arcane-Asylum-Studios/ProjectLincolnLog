24e140cb312884c1ed9f5d46fbeb97700f1cac2d
Nyx <bryceolcott@gmail.com>
2024-09-07 01:59:55 -0600

    remove houdini pluigins (#11)
    * Create README.md
    
    * created player character
    
    * Basic opening menu functionality, Basic Health Bar HUD
    
    * Basic Building/Checking to see if uploads to fork
    
    * Modified Door
    
    * Extra Models
    
    Just an upload of some house stuff.
    
    * Created Base Player model to start working on player script and Combat script.
    
    * Extra Stuff
    
    Just extra file uploads
    
    * Basic Building/Checking to see if uploads to fork
    
    * Modified Door
    
    * Extra Models
    
    Just an upload of some house stuff.
    
    * Created Base Player model to start working on player script and Combat script.
    
    * Create README.md
    
    * created player character
    
    * Basic opening menu functionality, Basic Health Bar HUD
    
    * removed houdini plugin
    
    ---------
    
    Co-authored-by: Elijah <10976947@uvu.edu>
    Co-authored-by: Seth C <monkeybean1464@gmail.com>
    Co-authored-by: Seth C <46496200+Monkeybean007@users.noreply.github.com>

--- Plugins/HoudiniEngine/Intermediate/Build/Win64/x64/UnrealGame/Shipping/HoudiniEngineRuntime/PCH.HoudiniEngineRuntime.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.35.32217
-#include "D:/builder-new/Nightly20.5CMake/dev/engine/unreal/5.4/Engine/Plugins/Runtime/HoudiniEngine/Intermediate/Build/Win64/x64/UnrealGame/Shipping/HoudiniEngineRuntime/PCH.HoudiniEngineRuntime.h"
