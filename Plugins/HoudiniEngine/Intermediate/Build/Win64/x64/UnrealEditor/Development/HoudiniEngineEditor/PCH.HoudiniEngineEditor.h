a918a80a62665ea8e4b00f1c295f1e4586e350f4
Nyx Olcott <bryceolcott@gmail.com>
2024-09-07 01:57:00 -0600

    removed houdini plugin

--- Plugins/HoudiniEngine/Intermediate/Build/Win64/x64/UnrealEditor/Development/HoudiniEngineEditor/PCH.HoudiniEngineEditor.h
+++
@@ -1,3 +0,0 @@
-// PCH for D:/GitHub/MainProjectLincolnLog/Plugins/HoudiniEngine/Source/HoudiniEngineEditor/Private/HoudiniEngineEditorPrivatePCH.h
-#include "D:/GitHub/MainProjectLincolnLog/Plugins/HoudiniEngine/Intermediate/Build/Win64/x64/UnrealEditor/Development/HoudiniEngineEditor/Definitions.h"
-#include "D:/GitHub/MainProjectLincolnLog/Plugins/HoudiniEngine/Source/HoudiniEngineEditor/Private/HoudiniEngineEditorPrivatePCH.h"
--- Plugins/HoudiniEngine/Intermediate/Build/Win64/x64/UnrealEditor/Development/HoudiniEngineRuntime/PCH.HoudiniEngineRuntime.h
+++
@@ -1,3 +0,0 @@
-// PCH for D:/GitHub/MainProjectLincolnLog/Plugins/HoudiniEngine/Source/HoudiniEngineRuntime/Private/HoudiniEngineRuntimePrivatePCH.h
-#include "D:/GitHub/MainProjectLincolnLog/Plugins/HoudiniEngine/Intermediate/Build/Win64/x64/UnrealEditor/Development/HoudiniEngineRuntime/Definitions.h"
-#include "D:/GitHub/MainProjectLincolnLog/Plugins/HoudiniEngine/Source/HoudiniEngineRuntime/Private/HoudiniEngineRuntimePrivatePCH.h"
