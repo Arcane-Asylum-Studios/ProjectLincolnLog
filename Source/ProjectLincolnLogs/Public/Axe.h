183f7fdd2988b3b41fd05463b4d839ebe663569d
Nyx Olcott <bryceolcott@gmail.com>
2024-09-28 19:20:50 -0600

    Removed 3rd person content. created some C++ classes

---
+++ Source/ProjectLincolnLogs/Public/Axe.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WeaponBase.h"
+#include "Axe.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLINCOLNLOGS_API AAxe : public AWeaponBase
+{
+	GENERATED_BODY()
+	
+};
